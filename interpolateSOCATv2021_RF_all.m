ef72aa7a1d8d933071fde93296fac6a490778936
Jon Sharp <jonathan.sharp@noaa.gov>
2021-09-30 12:13:41 -0700

    Update interpolateSOCATv2021_RF_all.m

--- interpolateSOCATv2021_RF_all.m
+++ interpolateSOCATv2021_RF_all.m
@@ -12,11 +12,10 @@ SOCATv2021_grid.month_cos = cos((2.*pi.*SOCATv2021_grid.month)./12);
 %% Predictor variables
 X = [SOCATv2021_grid.distance_from_shore(:) ...
      SOCATv2021_grid.SSS(:) ...
-     SOCATv2021_grid.SSH(:) ...
      SOCATv2021_grid.SST(:) ...
      log10(SOCATv2021_grid.CHL(:)) ...
      log10(SOCATv2021_grid.MLD(:)) ...
-     SOCATv2021_grid.wind_speed(:).^2 ...
+     SOCATv2021_grid.wind_speed(:) ...
      SOCATv2021_grid.pCO2_atm(:) ...
      SOCATv2021_grid.year_since_1997(:) ...
      SOCATv2021_grid.month_sin(:) ...
@@ -36,46 +35,9 @@ X = [SOCATv2021_grid.distance_from_shore(:) ...
 %      SOCATv2021_grid.bottomdepth(:)];
 
 % Predictor variable headers
-headers = {'Dist' 'SSS' 'SSH' 'SST' 'CHL' 'MLD' 'Wind' 'aCO2' 'Year' 'Month1' 'Month2'};
+headers = {'Dist.' 'SSS' 'SST' 'CHL' 'MLD' 'Wind' '{\itp}CO_{2(atm.)}' 'Year' 'sin(Mo.)' 'cos(Mo.)'};
 % Standard case:
 % headers = {'Dist' 'SSS' 'SSH' 'SST' 'CHL' 'MLD' 'Wind' 'aCO2' 'Year' 'Month1' 'Month2' 'Bathy'};
-
-% Import mooring data
-load('Data/mooring_data_processed.mat');
-Y_moor = [];
-idxlon = [];
-idxlat = [];
-idxmon = [];
-moornames = fieldnames(MOORING);
-for k=1:numel(moornames)
-    % Determine mooring month since 1998
-    MOORING.(moornames{k}).month_since_1998 = ...
-        (MOORING.(moornames{k}).date(:,1)-1998).*12 + MOORING.(moornames{k}).date(:,2);
-    % Determine means of each month since 1998
-    for m=1:max(SOCATv2021_grid.month_since_1998)
-        Y_moor = [Y_moor;mean(MOORING.(moornames{k}).pCO2SW(MOORING.(moornames{k}).month_since_1998==m),'omitnan')];
-        idxlon = [idxlon;find(abs(SOCATv2021_grid.longitude(:,1,1)-mean(MOORING.(moornames{k}).lon,'omitnan')) == ...
-            min(min(abs(SOCATv2021_grid.longitude(:,1,1)-mean(MOORING.(moornames{k}).lon,'omitnan')))))];
-        idxlat = [idxlat;find(abs(SOCATv2021_grid.latitude(1,:,1)-mean(MOORING.(moornames{k}).lat,'omitnan')) == ...
-            min(min(abs(SOCATv2021_grid.latitude(1,:,1)-mean(MOORING.(moornames{k}).lat,'omitnan')))))];
-        idxmon = [idxmon;m];
-    end
-end
-X_moor = [];
-for n = 1:size(Y_moor,1)
-X_moor = [X_moor;...
-     SOCATv2021_grid.distance_from_shore(idxlon(n),idxlat(n),idxmon(n)) ...
-     SOCATv2021_grid.SSS(idxlon(n),idxlat(n),idxmon(n)) ...
-     SOCATv2021_grid.SSH(idxlon(n),idxlat(n),idxmon(n)) ...
-     SOCATv2021_grid.SST(idxlon(n),idxlat(n),idxmon(n)) ...
-     log10(SOCATv2021_grid.CHL(idxlon(n),idxlat(n),idxmon(n))) ...
-     log10(SOCATv2021_grid.MLD(idxlon(n),idxlat(n),idxmon(n))) ...
-     SOCATv2021_grid.wind_speed(idxlon(n),idxlat(n),idxmon(n)).^2 ...
-     SOCATv2021_grid.pCO2_atm(idxlon(n),idxlat(n),idxmon(n)) ...
-     SOCATv2021_grid.year_since_1997(idxlon(n),idxlat(n),idxmon(n)) ...
-     SOCATv2021_grid.month_sin(idxlon(n),idxlat(n),idxmon(n)) ...
-     SOCATv2021_grid.month_cos(idxlon(n),idxlat(n),idxmon(n))];
-end
 
 %% Predictor variable combinations
 Index_var = true(size(X,2)+1,size(X,2));
@@ -102,7 +64,6 @@ X_train = X(Index_all,:);
 X_train = X_train(:,Index_var(v,:));
 X_test = X(Index_all,:);
 X_test = X_test(:,Index_var(v,:));
-X_moor_all = X_moor(:,Index_var(v,:));
 % Response variable (pCO2):
 Y_train = SOCATv2021_grid.all.pco2_ave_weighted(:);
 Y_train = Y_train(Index_all);
@@ -112,10 +73,11 @@ Y_test = Y_test(Index_all);
 %% Train Random Forest Model
 
 % Set parameters for RF model
-% rng(1); % For reproducibility
-nTrees = 1000;
-minLeafSize = 2;
+rng(20); % For reproducibility
+nTrees = 1200;
+minLeafSize = 5;
 numpredictors = 6;
+infrac = 1.0;
 
 % Leaf size test
 % disp('**Can uncomment this section to try different values of minLeafSize')
@@ -134,7 +96,7 @@ numpredictors = 6;
 % hold off
 
 % Construct RF model
-b = TreeBagger(nTrees,X_train,Y_train,'Method','regression','OOBPrediction','on',...
+b = TreeBagger(nTrees,X_train,Y_train,'Method','regression','OOBPrediction','on','InBagFraction',infrac,...
     'MinLeafSize',minLeafSize,'NumPredictorsToSample',numpredictors,'OOBPredictorImportance','on');
 
 % Plot Out-of-Bag MSE based on tree number
@@ -144,9 +106,12 @@ xlabel('Number of Grown Trees');
 ylabel('Out-of-Bag Root Mean Squared Error');
 
 % Plot importance of each predictor
-figure
-bar(b.OOBPermutedPredictorDeltaError);
-xlabel('Predictor') ;
+figure;
+set(gcf,'units','normalized','outerposition',[0 0 0.5 0.5]);
+set(gca,'fontsize',22);
+bar(b.OOBPermutedPredictorDeltaError,'k');
+set(gca,'fontsize',16);
+xlabel('Predictors') ;
 ylabel('Out-of-Bag Feature Importance');
 xticklabels(headers);
 
@@ -159,9 +124,6 @@ disp('Evaluate random forest model');
 RF_pred = predict(b,X_test);
 % Fit linear relationship between gridded and predicted pCO2
 mdl = fitlm(Y_test,RF_pred);
-
-RF_pred_moor = predict(b,X_moor_all);
-mdl_moor = fitlm(Y_moor,RF_pred_moor);
 
 % Plot measured vs. predicted pCO2 values
 figure; hold on;
@@ -176,42 +138,22 @@ ylim([100 1000]);
 c=colorbar; 
 c.Label.String = 'Relative data density';
 
-% Plot measured vs. predicted pCO2 values (moorings)
-figure; hold on;
-set(gca,'fontsize',16);
-colormap(cmocean('thermal'));
-scatter_kde(Y_moor, RF_pred_moor, 'filled', 'MarkerSize', 15); 
-plot([100 1000],[100 1000],'k--','LineWidth',2);
-xlabel('Mooring measured monthly mean pCO2','fontsize',16);
-xlim([100 1000]);
-ylabel('RFR-estimated pCO_{2}','fontsize',16);
-ylim([100 1000]);
-c=colorbar; 
-c.Label.String = 'Relative data density';
-
 % Determine error statistics
 err_RF = RF_pred-Y_test; % Errors
 rmse_RF = sqrt(mean(err_RF.^2)); % RMSE
-err_moor = RF_pred_moor-Y_moor; % Mooring Errors
-rmse_moor = sqrt(mean(err_moor.^2,'omitnan')); % Mooring RMSE
+%err_moor = RF_pred_moor-Y_moor; % Mooring Errors
+%rmse_moor = sqrt(mean(err_moor.^2,'omitnan')); % Mooring RMSE
 out_of_bag_error = oobError(b);
 fprintf('Mean of RF prediction: \n %f \n',mean(err_RF,'omitnan'));
 fprintf('RMSE from RF prediction: \n %f \n',rmse_RF);
 fprintf('R^2 of SOCAT~Predicted: \n %f \n',mdl.Rsquared.Ordinary);
-fprintf('RMSE from mooring predictions: \n %f \n',rmse_moor);
+%fprintf('RMSE from mooring predictions: \n %f \n',rmse_moor);
 fprintf('OOB RMSE: \n %f \n',sqrt(out_of_bag_error(end)));
 
 % Plot histogram
 edges = -200:0.5:200;
 figure; histogram(err_RF,edges); xlabel('Error'); ylabel('Number of Points');
 title('RF');
-
-Table_row_headers{v,:} = strjoin(headers_loop);
-Table(v,1) = mean(err_RF,'omitnan');
-Table(v,2) = rmse_RF;
-Table(v,3) = mdl.Rsquared.Ordinary;
-Table(v,4) = rmse_moor;
-Table(v,5) = sqrt(out_of_bag_error(end));
 
 end
 
@@ -244,7 +186,7 @@ SOCATv2021_grid.pco2_RF = SOCATv2021_grid.pco2_RF;
 %% Remove trend from time series using monthly domain mean pCO2
 % Calculate domain means
 disp('Removing trend from pCO2');
-area_weights = SOCATv2021_grid.area_km2;
+area_weights = SOCATv2021_grid.area_km2.*SOCATv2021_grid.percent_sea;
 area_weights = repmat(area_weights,1,1,size(SOCATv2021_grid.pco2_RF,3));
 area_weights(isnan(SOCATv2021_grid.pco2_RF)) = NaN;
 SOCATv2021_grid.pco2_RF_dom_mean = ...
