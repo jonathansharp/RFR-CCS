42aaa2446a91f734284ca81deca7859a1140e40f
Jon Sharp <jonathan.sharp@noaa.gov>
2021-09-30 12:08:36 -0700

    Update Figure2.m

--- Figure2.m
+++ Figure2.m
@@ -62,5 +62,5 @@ xlim([min(date(~isnan(MOORING.CCE2.pCO2SW_mon_mean))) ...
 ylim([300 550]);
 
 %% Export figure
-exportgraphics(gcf,'/Users/sharp/Desktop/Figure2.jpg');
+exportgraphics(gcf,'/Users/sharp/Desktop/Figure2.png');
 
